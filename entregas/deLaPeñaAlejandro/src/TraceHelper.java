c4747e530fbd7e123f6095fdb244e2ea8adb0f22
Alejandro De La Peña <alejandro.delapena@alumnos.uneatlantico.es>
2026-04-29 13:12:35 +0200

    Update: TraceHelper arreglado

--- entregas/deLaPeñaAlejandro/src/TraceHelper.java
+++ entregas/deLaPeñaAlejandro/src/TraceHelper.java
@@ -1,28 +1,10 @@
 public class TraceHelper {
-    // Para controlar la profundidad de la recursión
-    private static int depth = 0;
-
-    public static void indent() {
-        for (int i = 0; i < depth; i++) System.out.print("  ");
+    public static void printArray(int[] array, String mensaje) {
+        System.out.println(mensaje + " " + java.util.Arrays.toString(array));
     }
 
-    public static void enterRecursiveCall(String msg) {
-        indent();
-        System.out.println("-> Entrando: " + msg);
-        depth++;
-    }
-
-    public static void exitRecursiveCall(String msg) {
-        depth--;
-        indent();
-        System.out.println("<- Saliendo: " + msg);
-    }
-
-    public static String format(int[] arr) {
-        StringBuilder sb = new StringBuilder("[");
-        for (int i = 0; i < arr.size; i++) { // Dependiendo de si es array completo o sub-array
-            // Implementación simple de print
-        }
-        return java.util.Arrays.toString(arr);
+    public static void printArray(int[] array, String mensaje, int nivel) {
+        System.out.print("    ".repeat(nivel));
+        System.out.println(mensaje + " " + java.util.Arrays.toString(array));
     }
 }
